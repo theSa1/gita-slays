35801d88d158704b0a880917e969fc44ba9e3087
sa1 <i@sa1.dev>
2025-01-12 01:00:37 +0530

    style: update color variables in globals.css and enhance page layout in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,25 +5,39 @@ const Page = async () => {
   const chapters = await getChapters();
 
   return (
-    <div className="grid grid-cols-1 gap-4">
-      {chapters.map((chapter) => (
-        <Link
-          href={`/chapter/${chapter.id}`}
-          key={chapter.id}
-          className="block p-6 bg-white rounded-lg shadow-md hover:shadow-lg transition-shadow"
-        >
-          <h3 className="uppercase text-primary font-semibold mb-2">
-            Chapter {chapter.chapter_number}
-          </h3>
-          <p className="mb-1 leading-none">{chapter.name}</p>
-          <p className="text-xl font-bold">
-            {chapter.name_meaning_gen_z_translation}
-          </p>
-          <p className="mt-2 line-clamp-3">
-            {chapter.chapter_summary_gen_z_translation}
-          </p>
-        </Link>
-      ))}
+    <div>
+      <section className="h-[80vh] flex flex-col items-center justify-center">
+        <h1 className="sm:text-6xl text-4xl font-bold text-center">
+          Welcome to
+          <br />
+          <span className="bg-gradient-to-br from-primary to-pink-500 text-transparent bg-clip-text">
+            GitaSlays
+          </span>
+        </h1>
+        <p className="text-center mt-4">
+          Slaying Life’s Challenges, One Verse at a Time.
+        </p>
+      </section>
+      <section className="grid grid-cols-1 gap-4">
+        {chapters.map((chapter) => (
+          <Link
+            href={`/chapter/${chapter.id}`}
+            key={chapter.id}
+            className="block p-6 bg-card border rounded-lg shadow-md hover:shadow-lg transition-shadow"
+          >
+            <h3 className="uppercase text-primary font-semibold mb-2">
+              Chapter {chapter.chapter_number}
+            </h3>
+            <p className="mb-1 leading-none">{chapter.name}</p>
+            <p className="text-xl font-bold">
+              {chapter.name_meaning_gen_z_translation}
+            </p>
+            <p className="mt-2 line-clamp-3">
+              {chapter.chapter_summary_gen_z_translation}
+            </p>
+          </Link>
+        ))}
+      </section>
     </div>
   );
 };
